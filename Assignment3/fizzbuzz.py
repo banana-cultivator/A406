9efe8feed90e1a93fe1f2b0abb6d1e50fb2ed071
banana-cultivator <45516370+banana-cultivator@users.noreply.github.com>
2020-05-17 00:35:40 -0400

    Proposed changes for fizzbuzz

--- Assignment3/fizzbuzz.py
+++ Assignment3/fizzbuzz.py
@@ -9,3 +9,12 @@ def fizz_buzz(length):
         - if the index is a multiple of both three and five, the list element will be 'FizzBuzz'
         - otherwise, the list element will be a string of the value of the index
     '''
+    if length % 3 == 0:
+        return 'Fizz'
+    elif length % 5 == 0:
+        return 'Buzz'
+    elif length % 3 == 0 and length % 5 == 0:
+        return 'FizzBuzz'
+    else:
+        return length
+    
