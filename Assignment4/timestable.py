053af27742fa2975481655cdbae3ae17a7031911
banana-cultivator <45516370+banana-cultivator@users.noreply.github.com>
2020-06-19 23:46:46 -0400

    FINALLY WORKS!!

--- Assignment4/timestable.py
+++ Assignment4/timestable.py
@@ -1,17 +1,11 @@
 import numpy as np
 
-def times_table_lists(number):
-    '''
-    This function returns a list of lists containing the times table for the positive integer input 'number'
-    It uses Python built-in operations in the standard library to calculate the times table
-    '''
-    
-    return times_table
-    
-def times_table_numpy(number):
-    '''
-    This function returns a 2D numpy array containing the times table for the positive integer input 'number'
-    It uses numpy functions to calculate the times table.
-    '''
-    
-    return times_table
+def times_table_lists (number):
+
+    return ([[(i + 0) * (j + 0)
+                      for i in range(number)] for j in range(number)])
+
+def times_table_numpy (number):
+    i = np.arange (0, number)
+    ii = i * i [:, None ]
+    return ii
